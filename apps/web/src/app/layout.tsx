2b4a5d3925bd5560406cc370ddae7bfa013484d3
joshuaepstein <18082@jfs.brent.sch.uk>
2023-08-09 22:43:16 +0100

    fix: unused import and const

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,14 +1,8 @@
 import '@/styles/globals.css';
 import { cn } from '@mindplanr/utils';
-import { Inter } from 'next/font/google';
 import localFont from 'next/font/local';
 
 const rifficFree = localFont({ src: '../../public/_static/fonts/RifficFree-Bold.ttf', weight: 'bold' })
-
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-})
 
 export default function RootLayout({
   children,
